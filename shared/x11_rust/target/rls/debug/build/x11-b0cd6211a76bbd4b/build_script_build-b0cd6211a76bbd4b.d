541ce5f2caaddf4019d8a2e0d087bc6007b73539
zommiommy <tommaso.fontana.96@gmail.com>
2020-12-13 20:42:11 +0100

    incremental WIP, cleaned the repo

--- shared/x11_rust/target/rls/debug/build/x11-b0cd6211a76bbd4b/build_script_build-b0cd6211a76bbd4b.d
+++
@@ -1,5 +0,0 @@
-/Workspace/Github/rdwm/shared/x11_rust/target/rls/debug/build/x11-b0cd6211a76bbd4b/build_script_build-b0cd6211a76bbd4b: /home/zom/.cargo/registry/src/github.com-1ecc6299db9ec823/x11-2.18.2/build.rs
-
-/Workspace/Github/rdwm/shared/x11_rust/target/rls/debug/build/x11-b0cd6211a76bbd4b/build_script_build-b0cd6211a76bbd4b.d: /home/zom/.cargo/registry/src/github.com-1ecc6299db9ec823/x11-2.18.2/build.rs
-
-/home/zom/.cargo/registry/src/github.com-1ecc6299db9ec823/x11-2.18.2/build.rs:
